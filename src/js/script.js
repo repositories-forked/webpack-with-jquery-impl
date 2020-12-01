203f0fd161d7a2d7a929bb4fbc8ba9bab3b66047
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-12-01 11:01:35 +0530

    jQuery with import statment

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+// var $ = require('jquery');       << CommonJS
+import $ from 'jquery';             // ES6
+
 var color = 'yellow';
 $("button").click(function(){
     $("#my-div").css("background-color", color == 'blue'? (color = 'yellow'): (color = 'blue'));
